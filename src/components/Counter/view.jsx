51c5552e9c22a42f5553e1d5c82df345fbeb0bed
atomita <sleeping.cait.sith+gh@gmail.com>
2018-12-04 19:37:43 +0900

    feat: Apply xatto v1.0.0-rc.1

--- src/components/Counter/view.jsx
+++ src/components/Counter/view.jsx
@@ -1,14 +1,12 @@
 import { x } from 'xatto';
 
-const down = (event, context) => ({ count: context.count - 1 });
-const up = (event, context) => ({ count: context.count + 1 });
+const down = context => ({ count: context.count - 1 });
+const up = context => ({ count: context.count + 1 });
 
 export const CounterView = ({ xa: { context }, ...attrs }, children) => (
   <div class="c-counter">
     <h1>{context.count}</h1>
-    <button onclick={down} data-test="down-button">
-      -
-    </button>
+    <button onclick={down}>-</button>
     <button onclick={up}>+</button>
   </div>
 );
